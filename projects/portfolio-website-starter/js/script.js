4d0b4ff1358d5cee61d9d7fe801f34c9855d7b5f
Stefano Cerruti <stefano.cerruti@outlook.com>
2024-02-17 18:33:33 +0100

    Endind

--- projects/portfolio-website-starter/js/script.js
+++ projects/portfolio-website-starter/js/script.js
@@ -1,1 +1,17 @@
-// Add custom JavaScript here
+function typeText(text, typingElement, typingDelay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingElement.textContent += text.charAt(i);
+    }, typingDelay * i);
+  }
+}
+
+function runTypingEffect() {
+  const text = "I am dustech.";
+  const typingElement = document.getElementById("typing-text");
+  const typingDelay = 100;
+
+  typeText(text, typingElement, typingDelay);
+}
+
+document.addEventListener("DOMContentLoaded", runTypingEffect);
